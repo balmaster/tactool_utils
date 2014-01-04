7153888ffb966a047bac82c9765f6a9ae20321c8
Ruslan Markelov <markelov@jet.msk.su>
2014-01-05 02:02:30 +0400

    оказалось что GM_xmlhttpRequest не вызывается из обычных скриптов даже если ее туда передать

--- scripts/src/main/js/move_attacks.js
+++ scripts/src/main/js/move_attacks.js
@@ -7,13 +7,181 @@
  */
 
 (function (moveAttacks) {
+    var SEND_MIN_INTERVAL = 1000 * 1;
+    var SEND_INTERVAL = 1000 * 2;
+    var URL_TACTOOL_ATTACK_INPUT_VIEWER = "http://tactool.net/D/AttackInputViewer.php";
+
     moveAttacks.init = init;
+    var $;
+    var GM_xmlhttpRequest;
+    var userSettings;
 
-    function init($) {
-        $('#map_details .movements .cf').append('<button class="green" id="moveAttacksButton" value="Отправить в tac_tool" type="button"><div class="button-container addHoverClick "><div class="button-background"><div class="buttonStart"><div class="buttonEnd"><div class="buttonMiddle"></div></div></div></div><div class="button-content">Отправить в tac_tool</div></div></button>');
-
-
+    function init(params, _userSettings) {
+        $ = params.jquery;
+        GM_xmlhttpRequest = params.GM_xmlhttpRequest;
+        userSettings = _userSettings;
+        $('#map_details .movements .cf').append('<button class="green" id="moveAttacksButton" value="Отправить в tactool.net" type="button"><div class="button-container addHoverClick "><div class="button-background"><div class="buttonStart"><div class="buttonEnd"><div class="buttonMiddle"></div></div></div></div><div class="button-content">Отправить в tactool.net</div></div></button>');
+        $("#moveAttacksButton").click(function () {
+            handleAttacks();
+        });
     }
 
+
+    function ajaxRequest(url, options) {
+        if (!options.type) {
+            options.type = "POST";
+        }
+        if (!options.cache) {
+            options.cache = false;
+        }
+        if (options.type === "POST" && (!options.data || options.data.length === 0)) {
+            options.data = {timestamp: new Date().getTime()};
+        }
+
+        return $.ajax(url, options);
+    }
+
+    function htmlRequest(url, requestData, responseHandler) {
+
+        function parseResponse(result, textStatus, jqXHR) {
+            responseHandler($.parseHTML(result));
+        }
+
+        function transportError(result, textStatus, jqXHR) {
+            console.error("ошибка" + textStatus);
+        }
+
+        return ajaxRequest(url,
+            {
+                dataType: 'html',
+                data: requestData,
+                success: parseResponse,
+                error: transportError
+            });
+    }
+
+    function gmRequest(url, params, success, error) {
+        var options = {
+            url: url,
+            method: ( !params ? 'GET' : 'POST' ),
+            headers:{},
+            onload: success,
+            onerror: error,
+            onabort:function(e) {console.error(e);}
+        };
+        if (params) {
+            var data = '';
+            for (n in params) {
+                data += '&' + n + '=' + encodeURIComponent(params[n]);
+            }
+            options.headers["Content-type"] = "application/x-www-form-urlencoded";
+            options.headers["Content-length"] = data.length;
+            options.data = data;
+        }
+
+        GM_xmlhttpRequest(options);
+    }
+
+    function gmHtmlRequest(url, params, responseHandler) {
+        function parseResponse(response) {
+            responseHandler($.parseHTML(response.responseText));
+        }
+
+        function transportError(response) {
+            console.error("ошибка" + response);
+        }
+
+        return gmRequest(url, params, parseResponse, transportError);
+    }
+
+    function requestBuildPage(gid, filter, tt, page, handler) {
+        htmlRequest("build.php?gid=" + gid + "&filter=" + filter + "&tt=" + tt + "&page=" + page, undefined, function (resultDom) {
+            handler(resultDom);
+        });
+    }
+
+    function handleAttacks() {
+        var x = 97, y = -164;
+        var content = "";
+        var attacksCount = 0;
+
+        function sendContent() {
+            alert("Собрана информация о " + attacksCount + " нападениях. Нажмите на ок для загрузки.")
+            console.error(content);
+
+            gmHtmlRequest(URL_TACTOOL_ATTACK_INPUT_VIEWER,
+                {
+                    attacks: content,
+                    idL: userSettings.DEFLIST_ID,
+                    x: x,
+                    y: y
+                }, function (responseDom) {
+                    alert(responseDom);
+
+                    //gmHtmlRequest(userSettings.DEFLIST_URL + "&code=0", undefined, function (responseDom) {
+                    //    alert("Информация о " + attacksCount + " нападениях внесена.")
+                    //});
+                });
+        }
+
+        function handleAttacksPage(page) {
+            //console.error("handleAttacksPage" + page);
+            setTimeout(function () {
+                requestBuildPage(16, 1, 1, page, function (responseDom) {
+                    var currentPage = Number($(responseDom).find("#build .data .paginator .currentPage")[0].textContent);
+                    if (page <= currentPage) {
+
+                        var troopDetails = $(responseDom)
+                            .find("#build .data .troop_details")
+                            .filter(function () {
+                                var troopClass = $(this).attr("class");
+                                return troopClass.indexOf("inAttack") >= 0 || troopClass.indexOf("inRaid") >= 0;
+                            });
+
+                        //console.error("page " + page + " details " + troopDetails.length);
+                        $(troopDetails).each(function (i, troopDetail) {
+                            attacksCount++;
+
+                            $(troopDetail).find("thead td.role a").each(function (j, node) {
+                                content += node.textContent + "\t";
+                            });
+
+                            content += "Отметить нападение\t";
+
+                            $(troopDetail).find("thead td.troopHeadline a").each(function (j, node) {
+                                content += node.textContent + "\t";
+                            });
+
+                            content += "\n";
+
+                            $(troopDetail).find("tbody.units th.coords span span").each(function (j, node) {
+                                content += node.textContent;
+                            });
+
+                            $(troopDetail).find("tbody.units td.uniticon img").each(function (j, node) {
+                                content += $(node).attr("alt") + "\t";
+                            });
+
+                            content += "\n";
+                            content += "Войска 	? 	? 	? 	? 	? 	? 	? 	? 	? 	? 	?" + "\n";
+
+                            $(troopDetail).find("tbody.infos th").each(function (j, node) {
+                                content += node.textContent + "\n";
+                            });
+                            $(troopDetail).find("tbody.infos td div").each(function (j, node) {
+                                content += node.textContent + "\n";
+                            });
+                        });
+                        handleAttacksPage(page + 1);
+                    }
+                    else {
+                        sendContent();
+                    }
+                });
+            }, Math.floor(SEND_MIN_INTERVAL + SEND_INTERVAL * Math.random()));
+        }
+
+        handleAttacksPage(1);
+    }
 
 }(window.moveAttacks = window.moveAttacks || {}));
